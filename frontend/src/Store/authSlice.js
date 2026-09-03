fff8e8df7341a09e63fb3de4293a549e6bd70f5f
Anusha <anushadeshpande257@gmail.com>
2026-09-03 12:02:03 +0530

    added state management for auth

---
+++ frontend/src/Store/authSlice.js
@@ -0,0 +1,30 @@
+import {createSlice} from "@reduxjs/toolkit"
+
+const initialState = {
+
+    status: false,
+    userData: null,
+    loading: true
+
+}
+
+const authSlice = createSlice ({
+
+    name: "auth",
+    initialState,
+    reducers: {
+        login: (state, action) => {
+            state.status = true,
+            state.userData = action.payload.userData,
+            state.loading = false
+        },
+        logout: (status, action) => {
+            state.status = false,
+            state.userData = null,
+            state.loading = false
+        }
+    }
+})
+
+export const {login, logout} = authSlice.actions
+export default authSlice.reducer
